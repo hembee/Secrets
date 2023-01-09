ce6cab6db669c20768746a17aa7e59179c94d8a9
hembee <hembee999@gmail.com>
2023-01-09 22:37:00 +0100

    bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -23,8 +26,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = process.env.SECRET
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+// ENCRYPTION
+
+// const secret = process.env.SECRET;
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -34,7 +39,6 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 app.get("/login", (req, res) => {
-
   res.render("login");
 });
 app.get("/register", (req, res) => {
@@ -44,31 +48,38 @@ app.get("/register", (req, res) => {
 // POST REQUESTS
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, (req, result) => {
+          if (result === true) {
+            res.render("secret");
+          }
+        });
+
+        // if (foundUser.password === password) {
+        //   res.render("secrets");
+        // }
       }
     }
   });
